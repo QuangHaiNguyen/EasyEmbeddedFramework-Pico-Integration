b34e93017f7588331b24116ae9019af93ce4b281
Hai Nguyen <hainguyen.eeit@gmail.com>
2024-08-04 23:26:12 +0200

    application can config to use test worker or rtos

--- src/main.c
+++ src/main.c
@@ -6,25 +6,25 @@
 #include "pico/binary_info.h"
 #include "hardware/uart.h"
 
-#define DEBUG_LVL   LVL_TRACE      /**< logging level */
-#define MOD_NAME    "MAIN"  /**< module name */
+#define DEBUG_LVL   LVL_TRACE   /**< logging level */
+#define MOD_NAME    "MAIN"      /**< module name */
 #include "ez_logging.h"
 
 #include "ez_easy_embedded.h"
+#if (EZ_TASK_WORKER_ENABLE == 1)
 #include "ez_task_worker.h"
+#include "ez_freertos_port.h"
+#else
 #include "FreeRTOS.h"
 #include "task.h"
-#include "ez_freertos_port.h"
+#endif
 
-#define UART_ID uart0
-#define BAUD_RATE 115200
 
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
-
-#define BUFF_SIZE   256
-#define PRIORITY    10
-#define STACK_SIZE  512
+#define BUFF_SIZE           256
+#define PRIORITY            10
+#define STACK_SIZE          512
+#define WORKER1_DELAY_MS    10
+#define WORKER2_DELAY_MS    2000
 
 #define SUM_RESULT_EVENT    1
 
@@ -37,6 +37,7 @@
 * Module Variable Definitions
 *******************************************************************************/
 
+#if (EZ_TASK_WORKER_ENABLE == 1)
 typedef struct
 {
     int a;
@@ -44,37 +45,56 @@ typedef struct
 }Worker1_SumContext;
 
 static struct ezTaskWorkerThreadInterfaces *freertos_interfaces = NULL;
-static INIT_WORKER(worker1, 10, PRIORITY, STACK_SIZE);
-static INIT_WORKER(worker2, 2000, PRIORITY, STACK_SIZE);
+static INIT_WORKER(worker1, WORKER1_DELAY_MS, PRIORITY, STACK_SIZE);
+static INIT_WORKER(worker2, WORKER2_DELAY_MS, PRIORITY, STACK_SIZE);
 static uint8_t buff2[BUFF_SIZE] = {0};
 static uint8_t buff1[BUFF_SIZE] = {0};
+#endif /* EZ_TASK_WORKER_ENABLE */
 
 const uint LED_PIN = 25;
 
 /******************************************************************************
 * Function Definitions
 *******************************************************************************/
+#if (EZ_TASK_WORKER_ENABLE == 1)
 INIT_THREAD_FUNCTIONS(worker1);
 INIT_THREAD_FUNCTIONS(worker2);
 
 static bool worker1_SumService(int a, int b,  ezTaskWorkerCallbackFunc callback);
 static bool worker1_HandleSumService(void *context, ezTaskWorkerCallbackFunc callback);
 static void worker2_Callback(uint8_t event, void *ret_data);
+#else
+void vTask1()
+{
+    while(1)
+    {
+        EZDEBUG("Task1");
+        vTaskDelay(500);
+    }
+}
 
-
+void vTask2()
+{
+    while(1)
+    {
+        EZDEBUG("Task2");
+        vTaskDelay(1000);
+    }
+}
+#endif /* EZ_TASK_WORKER_ENABLE */
 
 int main()
 {
     bi_decl(bi_program_description("This is a test binary."));
     bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
     stdio_init_all();
-
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    bool ret = false;
-
     ezEasyEmbedded_Initialize();
+
+#if (EZ_TASK_WORKER_ENABLE == 1)
+    bool ret = false;
     ezFreeRTOSPort_Init();
     freertos_interfaces = ezFreeRTOSPort_GetInterface();
     ret = (freertos_interfaces != NULL);
@@ -100,13 +120,18 @@ int main()
                                          GET_THREAD_FUNC(worker2));
     }
     vTaskStartScheduler();
+#else
+    xTaskCreate(vTask1, "vTask1", 128, NULL, 1, NULL);
+    xTaskCreate(vTask2, "vTask2", 128, NULL, 1, NULL);
+    vTaskStartScheduler();
+#endif
 }
 
 
 /******************************************************************************
 * Internal functions
 *******************************************************************************/
-
+#if (EZ_TASK_WORKER_ENABLE == 1)
 THREAD_FUNC(worker1)
 {
     ezTaskWorker_ExecuteTask(&worker1, EZ_THREAD_WAIT_FOREVER);
@@ -124,7 +149,6 @@ THREAD_FUNC(worker2)
         EZINFO("Call sum service success");
     }
 }
-
 
 static bool worker1_SumService(int a, int b, ezTaskWorkerCallbackFunc callback)
 {
@@ -173,6 +197,10 @@ static void worker2_Callback(uint8_t event, void *ret_data)
         break;
     
     default:
+        EZINFO("Unknown event");
         break;
     }
 }
+#endif
+
+/* End of file */
